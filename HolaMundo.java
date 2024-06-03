72e6601d06ddc21d16ad7da26af459e68b90f7de
Keyner15 <keinermr0115@gmail.com>
2024-06-03 15:35:03 -0600

    Cambios en la APP de la rama A

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,21 @@
 package HolaMundo;
-
+import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("que mensaje deseas ver:\n 1-Hola\n 2-Mundo");
+        int dato = scanner.nextInt();
 
-        System.out.println("Hola Mundo");
+        if (dato == 1) {
+            System.out.println("Hola ");
+        }else if (dato == 2) {
+            System.out.println("Mundo");
+        }else{
+            System.out.println("esa opcion no existe");
+        }
+
+        
+        scanner.close();
     }
+    
 }
